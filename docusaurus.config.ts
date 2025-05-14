2416d12bcf55fc1e558d30e3497d8f86f85bea73
Daniel Leroux <daniel.leroux@siemens.com>
2025-05-14 14:13:09 +0200

    remove figma images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import figmaPlugin from './plugins/figma-pictures/figma';
 import path from 'path';
 import { themes as prismThemes } from 'prism-react-renderer';
 import versionDeployment from './version-deployment.json' with { type: 'json ' };
-
+import 'dotenv/config';
 function getAnnouncementBarConfig() {
   const latestVersion = versionDeployment.versions.find(version => version.id === versionDeployment.currentVersion);
 
