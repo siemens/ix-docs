46d6f602cd8aecdf23c865a8d0f6d29be491fada
Daniel Leroux <daniel.leroux@siemens.com>
2025-05-14 12:11:27 +0200

    copy theme

--- scripts/copy-theme.ts
+++ scripts/copy-theme.ts
@@ -11,18 +11,26 @@ export default function copyTheme() {
     const __nodeModule = require.resolve('@siemens-ix/corporate-theme');
     const __from = path.join(__nodeModule, '..');
 
+    const __preview = path.join(__dirname, 'static', 'demo', 'v2', 'preview');
+
     const __htmlPreview = path.join(
-      __dirname,
-      'static',
-      'demo',
-      'v2',
-      'preview',
+      __preview,
       'html',
       'additional-theme',
       'corporate-theme',
       'dist'
     );
+
+    const __htmlMobile = path.join(
+      __preview,
+      'mobile',
+      'additional-theme',
+      'corporate-theme',
+      'dist'
+    );
+
     fs.copySync(__from, __htmlPreview);
+    fs.copySync(__from, __htmlMobile);
 
     console.log('Found optionalDependency @siemens-ix/corporate-theme.');
   } catch (e) {
