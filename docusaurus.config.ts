37fc6bd4d7ddc7a90964525328639b48298014ed
Daniel Leroux <daniel.leroux@siemens.com>
2025-11-06 18:06:04 +0100

    chore: update version of brand and add news pill

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -232,6 +232,12 @@ const config: Config = {
           label: 'Styles',
         },
         {
+          type: 'custom-news-pill',
+          position: 'left',
+          label: 'Release 4.0 is here',
+          value: '/blog/2025/11/10/release-4',
+        },
+        {
           type: 'custom-version-selection',
           position: 'right',
           value: versionDeployment
