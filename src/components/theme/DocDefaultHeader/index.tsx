93850cffccb718ed87227622b057158951bccc51
Daniel Leroux <daniel.leroux@siemens.com>
2026-03-11 09:45:55 +0100

    feat: add ask ai button (#195)

--- src/components/theme/DocDefaultHeader/index.tsx
+++ src/components/theme/DocDefaultHeader/index.tsx
@@ -1,10 +1,16 @@
 /*
  * COPYRIGHT (c) Siemens AG 2018-2024 ALL RIGHTS RESERVED.
  */
-import { useScrollPosition } from '@docusaurus/theme-common/internal';
+import {
+  useLocalPathname,
+  useScrollPosition,
+} from '@docusaurus/theme-common/internal';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { IxTooltip } from '@siemens/ix-react';
 import { DeprecatedTag, RedirectTag } from '@site/src/components/UI/Tags';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import AskAI from '../AskAI';
 import styles from './styles.module.css';
 
 function Separator() {
@@ -30,20 +36,37 @@ export default function DocDefaultHeader(props: {
   frontMatter: any;
 }) {
   const { description, title, frontMatter, id } = props;
+  const { siteConfig } = useDocusaurusContext();
+  const pathname = useLocalPathname();
+
+  const prompt = useMemo(
+    () => `Use web browsing to access links and information ${siteConfig.url + pathname + '.md'}
+
+If you need additional information you find a overview of all content here: https://ix.siemens.io/llms.txt
+
+I want to ask some questions`,
+    [pathname, siteConfig.url]
+  );
 
   return (
     <>
-      <h1 className={styles.sticky_h1}>
-        {title}
+      <div className={styles.sticky_h1}>
+        <h1>
+          {title}
 
-        <a
-          href={`#${id.replaceAll('/', '-')}`}
-          className="hash-link"
-          aria-label={title}
-          title={title}
-        ></a>
-      </h1>
+          <a
+            href={`#${id.replaceAll('/', '-')}`}
+            className="hash-link"
+            aria-label={title}
+            title={title}
+          ></a>
+        </h1>
+        <AskAI id="copy-prompt" prompt={prompt} />
+      </div>
 
+      <IxTooltip for="#copy-prompt">
+        Copy a prompt to ask an AI assistant about this page.
+      </IxTooltip>
       {description && (
         <div className={clsx(styles.componentHeroHeader, 'HeroHeader')}>
           <div className={styles.Tags}>
