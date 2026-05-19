bf6a341f15180040135e457bcc8f22228ae83dd8
Alex Kaduk <o.kaduk@gmail.com>
2026-05-19 17:31:59 +0300

    fix(drawer): remove legacy drawer documentation  IX-4270 (#237)

--- sidebars.ts
+++ sidebars.ts
@@ -1147,23 +1147,6 @@ const sidebars: SidebarsConfig = {
       ],
       collapsed: true,
     },
-    {
-      type: 'category',
-      className: 'doc-sidebar-item-bold',
-      label: 'Deprecated',
-      items: [
-        createTabItem({
-          id: 'legacy/drawer/index',
-          label: 'Drawer',
-          items: [
-            {
-              id: 'legacy/drawer/code',
-              label: 'Code',
-            },
-          ],
-        }),
-      ],
-    },
   ],
 
   guidelines: [
